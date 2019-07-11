a52408d141e67aa9681606022ded951228722407
yoshi0518 <nakamura@n-asset.com>
2019-07-11 17:20:29 +0900

    Vuexの設定

--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count: 0
+  },
+  getters: {
+    count (state) {
+      return state.count
+    }
   },
   mutations: {
-
+    increment (state, payload) {
+      state.count += payload.cnt * 1
+    }
   },
   actions: {
-
+    increment ({ commit }, payload) {
+      commit('increment', payload)
+    }
   }
 })
