e050cf17edd4ce443e0769b81855090642005158
yoshi0518 <nakamura@n-asset.com>
2019-07-11 17:30:12 +0900

    ESLintの導入・設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'standard'
+  ],
+  plugins: [
+    'vue'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'generator-star-spacing': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
