303033ba63792cbe478d7ed598269838c080197f
yoshi0518 <nakamura@n-asset.com>
2019-08-04 19:20:34 +0900

    Elementをインストール

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
+
+Vue.use(ElementUI, { locale })
 
 new Vue({
   router,
